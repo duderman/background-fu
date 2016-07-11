6a660ed5344c54c1c25e951a3e9a01b249411530
Nikolay Norkin <zduderman@gmail.com>
2016-07-11 14:44:24 +0400

    rails require

--- lib/background_fu.rb
+++ lib/background_fu.rb
@@ -1,19 +1,18 @@
 module BackgroundFu
-  
-  VERSION = "1.0.10"
+  VERSION = "1.0.11"
   CONFIG_FILE = "config/background.yml"
   CONFIG = File.exist?(CONFIG_FILE) && YAML::load_file(CONFIG_FILE)['background_fu'] || {}
   CONFIG['cleanup_interval'] ||= :on_startup
   CONFIG['monitor_interval'] ||= 10
-  
 end
 
 require 'background_fu/job'
 require 'background_fu/job/bonus_features'
 require 'background_fu/worker_monitoring'
-require 'background_fu/railtie' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+require 'background_fu/railtie' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
 
 Job.send(:include, Job::BonusFeatures)
 
-Dir["lib/workers/*_worker.rb"].each { |f| require f } 
-
+path = "lib/workers/*_worker.rb"
+path = "#{Rails.root.to_s}/#{path}" if defined?(Rails) && Rails::VERSION::MAJOR >= 3
+Dir[path].each { |f| require f }
